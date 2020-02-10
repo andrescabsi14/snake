6273ad5bae41bad38aec36523f6425d3876d4157
Andres Cabrera <acabrera@scout.cool>
2020-02-10 09:42:24 -0500

    Interval partial

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -13,8 +13,62 @@ class Grid extends React.Component {
   state = {
     col: 0,
     boxes: [...Array(totalBoxes)].map((_, index) => index + 1),
-    head: [1, 1] // max value is given by totalBoxes
+    head: [1, 1], // max value is given by totalBoxes
+    direction: "right"
   };
+
+  moveTop = () => {};
+
+  moveRight = () => {};
+
+  moveBottom = () => {};
+
+  moveLeft = () => {};
+
+  handleControls(event) {
+    const top = 38;
+    const right = 39;
+    const bottom = 40;
+    const left = 37;
+
+    let direction;
+
+    switch (event.keyCode) {
+      case top:
+        console.log("top");
+        direction = "top";
+        return;
+      case right:
+        console.log("right");
+        direction = "right";
+
+        return;
+      case bottom:
+        console.log("bottom");
+        direction = "bottom";
+        return;
+      case left:
+        console.log("left");
+        direction = "left";
+        return;
+      default:
+        break;
+    }
+    this.interval = setInterval(this.autoMove, 100);
+  }
+
+  autoMove = () => {
+    console.log("automove");
+    this.setState({ head: [this.state.head[0], this.state.head[1] + 1] });
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleControls);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     const { boxes, head } = this.state;
@@ -23,6 +77,7 @@ class Grid extends React.Component {
       <div
         className="Grid-container"
         style={{ width: `${gridSize}px`, height: `${gridSize}px` }}
+        onKeyDown={e => this.handleControls(e)}
       >
         {boxes.map((box, index) => (
           <Box
